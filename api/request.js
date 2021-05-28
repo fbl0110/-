42776223d6ae7473ba6eed91a6d397c05ba16b04
zhangjunguang <845422129@qq.com>
2021-05-28 18:21:14 +0800

    28号

---
+++ api/request.js
@@ -0,0 +1,17 @@
+module.exports=(options)=>{
+  return new Promise((resolve,reject)=>{
+    wx.showLoading({
+      title: '数据加载中',
+    })
+    wx.request({
+      ...options,
+      success:function(res){
+        resolve(res.data)
+      },
+      fail:reject,
+      complete(){
+        wx.hideLoading()
+      }
+    })
+  })
+}
---
+++ packC/pages/details/index.js
@@ -0,0 +1,81 @@
+// pages/details/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    index:6,
+    show:false,
+    imageURL:'https://i.postimg.cc/GhxFkRC3/image.jpg',
+
+  },
+  showPopup() {
+    this.setData({ show: true });
+  },
+
+  onClose() {
+    this.setData({ show: false });
+  },
+  buy(){
+    wx.navigateTo({
+      url: '/packA/pages/confim/index',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
